c171aa9dcd83e61e2a8f4438411c722075dfa5fc
wuixwui <seoweet21@gmail.com>
2021-08-09 17:12:14 +0900

    feat: DOM API
    +querySelectorAll로 받아온 속성값들은 유사배열 형태로 저장되어
    고차배열 함수를 사용할 수 있습니다.

---
+++ 02.JS_Practice/app.js
@@ -0,0 +1,27 @@
+let boxEl = document.querySelector(".box");
+
+console.log(boxEl);
+
+boxEl.addEventListener("click", (handler) => {
+  console.log("clicked!");
+});
+
+boxEl.classList.add("active");
+let isConatins = boxEl.classList.contains("active");
+console.log(isConatins);
+
+boxEl.classList.remove("active");
+isConatins = boxEl.classList.contains("active");
+console.log(isConatins);
+
+const boxEls = document.querySelectorAll(".box");
+console.log(boxEls);
+
+// querySelectorAll로 찾아진 요소들은 유사배열 형태로 저장하게 됩니다.
+
+boxEls.forEach((boxEl, index) => {
+  boxEl.classList.add(`order-${index + 1}`);
+  console.log(index, boxEl);
+});
+
+boxEl.innerText = "box~";
