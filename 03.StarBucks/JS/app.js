edc8b64f2fb5ae57b648bddb380242fc18ffd7a8
wuixwui <seoweet21@gmail.com>
2021-08-18 20:37:06 +0900

    feat: Separate into common modules

--- 03.StarBucks/JS/app.js
+++ 03.StarBucks/JS/app.js
@@ -1,19 +1,3 @@
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-
-searchEl.addEventListener("click", function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener("focus", function () {
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색");
-});
-searchInputEl.addEventListener("blur", function () {
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
-});
-
 const badgeEl = document.querySelector("header .badges");
 const toTopEl = document.getElementById("to-top");
 
@@ -137,6 +121,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelector(".this-year");
-thisYear.innerText = new Date().getFullYear();
