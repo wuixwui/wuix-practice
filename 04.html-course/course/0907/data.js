3edd7a4786c8cd00e5894c2b7beacd871425f2ec
wuixwui <seoweet21@gmail.com>
2021-09-07 21:51:50 +0900

    feat: Append list using JSON

---
+++ 04.html-course/course/0907/data.js
@@ -0,0 +1,31 @@
+var tabData = {
+  lastModify: '2019-01-07',
+  useType: 'sub page',
+  tabItems: [
+    {
+      itemName: '학교',
+      itemColor: 'red',
+      itemContent: '학교에 관한 내용들입니다.',
+    },
+    {
+      itemName: '공원',
+      itemColor: 'black',
+      itemContent: '공원에 관한 내용들입니다.',
+    },
+    {
+      itemName: '놀이터',
+      itemColor: 'blue',
+      itemContent: '놀이터에 관한 내용들입니다.',
+    },
+    {
+      itemName: '집안',
+      itemColor: 'violet',
+      itemContent: '놀이터에 관한 내용들입니다.',
+    },
+    {
+      itemName: '사무실',
+      itemColor: 'green',
+      itemContent: '놀이터에 관한 내용들입니다.',
+    },
+  ],
+};
